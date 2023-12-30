235d030949f361f3a539a56359bbc1cec182791d
Sam Chung <sam.chung@peachystudio.com>
2023-12-30 12:37:25 -0800

    lsp and zen mode tmux hide

--- after/plugin/zen-mode.lua
+++ after/plugin/zen-mode.lua
@@ -32,7 +32,7 @@ require'zen-mode'.setup {
     },
     twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
     gitsigns = { enabled = false }, -- disables git signs
-    tmux = { enabled = false }, -- disables the tmux statusline
+    tmux = { enabled = true }, -- disables the tmux statusline
     -- this will change the font size on kitty when in zen mode
     -- to make this work, you need to set the following kitty options:
     -- - allow_remote_control socket-only
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -47,9 +47,6 @@ local plugins = {
             vim.o.timeoutlen = 300
         end,
         opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
         }
     },
     {
