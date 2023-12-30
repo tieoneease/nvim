841448c0499494e39c913681149c429834ace41f
Sam Chung <sam.chung@peachystudio.com>
2023-12-30 13:05:56 -0800

    more special sauce

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,6 +12,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Pasting from the void
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+--vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
 -- Windowpanes
 vim.o.splitbelow		= true
 vim.o.splitright		= true
