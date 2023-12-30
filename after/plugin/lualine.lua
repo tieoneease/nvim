7592f70682b9d7afe9ed040efc0913bfa462ced8
Sam Chung <sam.chung@peachystudio.com>
2023-12-30 05:11:29 -0800

    remove redundant mode lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -21,13 +21,7 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {},
-    lualine_x = {
-        {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-        }
-    },
+    lualine_x = {},
     lualine_y = {'buffers'},
     lualine_z = {'location'}
   },
