72bdbd82c65fe0ae4912d9aa498ffd2615796d3a
Sam Chung <sam.chung@peachystudio.com>
2023-12-30 02:38:42 -0800

    add lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,6 +16,13 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
 	'mbbill/undotree',
 	{ "RRethy/nvim-base16", version=false },
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
